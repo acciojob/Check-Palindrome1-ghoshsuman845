e9c3d6c433801a3091d7852325a24a4f4d2795b8
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-06-15 16:30:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(str) {
+  let start = 0;
+  let end = str.length - 1;
 
+  while (end > start) {
+    if (str[start] !== str[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+
+  return true;
 }
+
 module.exports = palindrome
